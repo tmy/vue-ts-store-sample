c1677dcb77fd6ba55f88c6e9183a8cc1ee985147
Akinori Tomita <akinori.tomita@gmail.com>
2020-04-28 01:14:38 +0900

    vue add vue-next

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     },
   },
   extends: [
-    'plugin:vue/recommended',
+    'plugin:vue/vue3-recommended',
     '@vue/airbnb',
     '@vue/typescript',
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
-import Vue from 'vue';
+import Vue, { createApp } from 'vue';
 import VueCompositionApi from '@vue/composition-api';
 import App from './App.vue';
 import globalStore from './stores/global';
 
-Vue.use(VueCompositionApi);
 Vue.prototype.$store = globalStore();
-Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+createApp(App).mount('#app');
